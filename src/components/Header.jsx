40faa305d19a22013bfdfec02ee79a6af53d5474
Sky274SS <kadetka274@gmail.com>
2022-01-18 22:49:11 +0300

    skymirs_web_day_1

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className='header'>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Tu-sofia-logo.svg/1200px-Tu-sofia-logo.svg.png"
+                    alt="logo"
+                    className='logo_img'/>
+            </header>
+    );
+};
+
+export default Header;
